672f1146bfb08fe7aa688fc2e9f1b3b73c4c2c66
sublte reference <subtlereference@gmail.com>
2023-03-25 03:18:53 -0400

    create users and login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,84 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
-const loginUser = (req, res) => {
-    res.send('Login Route')
+// @desc Register a new user
+// @route /api/users
+// @access Public 
+
+
+const registerUser =asyncHandler(async (req, res) => {
+    const {name, email, password} = (req.body)
+
+    //Validation
+    if(!name || !email || !password){
+         res.status (400)
+         throw new Error('Please include all fields')
+
+    }
+
+    // Find if user already exists
+    const userExists = await User.findOne({email})
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = await User.create({
+        name, 
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(User._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+})
+
+// @desc Login a new user
+// @route /api/users/login
+// @access Public 
+const loginUser = asyncHandler( async (req, res) => {
+    // pull in the email and password from the body of the request
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+    // Check user and password match
+    if(user && (await bcrypt.compare(password, user.hashedPassword))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(User._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid credentials(email or password)')
+    }
+
+    
+})
+
+// Generate Token function
+const generateToken =  (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
 }
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 const express = require('express')
+const colors = require('colors')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
 
+
+// Conenct to database
+connectDB()
+
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) =>{
     res.status(200).json({message: 'Welcome to the Support Desk API'})
 })
 
+
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
