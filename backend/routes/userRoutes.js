54af2a157cbae87255f3d6d9b56b2b7a3c36556b
sublte reference <subtlereference@gmail.com>
2023-05-01 15:44:12 -0400

    Can't figure out the 404 error

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const { registerUser, loginUser, getMe } = require('../controllers/userController')
-const { protect } = require('../middleware/authMiddleware')
+const express = require("express")
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController')
+const { protect } = require("../middleware/authMiddleware")
 const router = express.Router()
 
 router.post('/', registerUser)
@@ -8,4 +12,4 @@ router.post('/', registerUser)
 router.post('/login', loginUser)
 router.get('/me', protect, getMe)
 
-module.exports = router
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 
 // Conenct to database
@@ -15,13 +15,14 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) =>{
+app.get('/', (_, res) =>{
     res.status(200).json({message: 'Welcome to the Support Desk API'})
 })
 
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 app.use(errorHandler)
 
